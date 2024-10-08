cbc722353c297109de52624048a8708ecd0de09a
Glenn Renfro <grenfro@vmware.com>
2024-10-08 10:21:31 -0400

    Provide Spring Actuator Sample Application

---
+++ src/main/java/io/spring/actone/ActoneApplication.java
@@ -0,0 +1,13 @@
+package io.spring.actone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActoneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/spring/actone/HelloWorldController.java
@@ -0,0 +1,13 @@
+package io.spring.actone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
